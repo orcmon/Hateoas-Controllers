3393a9504d1e0779c718986ce5011a099e3ff0c7
orcmom <orcmon21@gmail.com>
2020-02-08 17:02:47 +0200

    Import spring project.

---
+++ src/main/java/orcmon/webdev/hateoasextended/HateoasExtendedApplication.java
@@ -0,0 +1,13 @@
+package orcmon.webdev.hateoasextended;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasExtendedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasExtendedApplication.class, args);
+	}
+
+}
